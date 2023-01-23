0a59100b8d489640ea7b48a27b8c2e55f82ccc72
Aurelien Roguet <>
2023-01-23 14:54:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+a=("Quelle est votre note du premier examen ?")
+b=("Quelle est votre note du deuxième examen ? ")
+if a==9 and b == 9 or (a+b)/2>=10 and (a>=8 or b>=8):
+  print("Vous êtes admis")
+else :
+  print(" Vous n'êtes pas admis")
